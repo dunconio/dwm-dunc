19b4478b4c80c4ad44fdfe762018610d8784db90
dunconio <dunconio1@gmail.com>
2024-07-18 16:38:38 +0100

    Revert "Add PATCH_BIDIRECTIONAL_TEXT"
    This reverts commit 0df434174f1e401fff3c25bb1871e373fe83f4ed.

--- config.mk
+++ config.mk
@@ -22,13 +22,9 @@ FREETYPEINC = /usr/include/freetype2
 #MANPREFIX = ${PREFIX}/man
 #KVMLIB = -lkvm
 
-# bi-directional text support
-BDINC = -I/usr/include/fribidi
-BDLIBS = -lfribidi
-
 # includes and libs
-INCS = -I${X11INC} -I${FREETYPEINC} ${BDINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} ${BDLIBS} -lX11-xcb -lxcb -lxcb-res ${KVMLIB} -lXrender -lImlib2 -lm -lXfixes -lXi -lXext -lXcomposite
+INCS = -I${X11INC} -I${FREETYPEINC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-res ${KVMLIB} -lXrender -lImlib2 -lm -lXfixes -lXi -lXext -lXcomposite
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
